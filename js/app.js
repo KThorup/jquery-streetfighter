85b2ab5cf79000c1da3c659af849e3de8d73e6d5
KarenThorup <karen.thorup@octanner.com>
2014-10-13 19:13:58 -0600

    streetfight-final

--- js/app.js
+++ js/app.js
@@ -1,29 +1,67 @@
-$(document).ready(function(){
-	$('.ryu').on('mouseenter', function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.on('mouseleave', function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	 .on('mousedown', function() {
-	 	playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').finish().show()
- 	 .animate({'left': '300px'}, 500, function() {
-    		$(this).hide();
-    		$(this).css('left', '-212px');
-  		});
- 	})
-	.on('mouseup', function() {
-		$('.ryu-throwing').hide();
-		$('.hadouken').hide();
-		$('.ryu-ready').show();
+$(document).ready(function() {
+	$('body').keydown(function(event) {
+		event.preventDefault();
+		if (event.which == 88) 
+			{ 	displayCool();	}
 	});
-	
+	$('body').keyup(function(event) {
+		event.preventDefault();
+		if (event.which == 88) 
+			{ 	displayStill();	}
+	});
+
+
+
+	$('.ryu').mouseenter(function() { displayReady();    })
+		     .mouseleave(function() { displayStill();    })
+		     .mousedown(function()  { 
+	     		playHadouken();
+		     	displayThrowing();
+		     	$('.hadouken').finish().show()
+		         .animate(
+      				{'left': '300px'}, 500, 
+      				function() {
+        				$(this).hide();
+        				$(this).css('left', '-212px');
+      				}
+    				);
+		     })
+			 .mouseup(function()    { displayReady();    });
+
 });
+
+function displayStill()
+{
+	hideAll();
+	$('.ryu-still').show();
+}
+
+function displayReady()
+{
+	hideAll();
+	$('.ryu-ready').show();
+}
+
+function displayThrowing()
+{
+	hideAll();
+	$('.ryu-throwing').show();	
+}
+
+function displayCool()
+{
+	hideAll();
+	$('.ryu-cool').show();
+}
+
+function hideAll()
+{
+	$('.ryu-still').hide();
+	$('.ryu-ready').hide();
+	$('.ryu-throwing').hide();
+	$('.ryu-cool').hide();
+	$('.hadouken').hide();
+}
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
